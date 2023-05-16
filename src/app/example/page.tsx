58914734fc21cd48bb0e0f57eb1d3d67cd7dc40f
Koji Uegaki <kjugkjk@gmail.com>
2023-05-16 14:20:41 +0900

    Route Handler を追加

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useState } from "react";
+
 export default function Example() {
-  return <h1>Example</h1>;
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    fetch("/api/example")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.text);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Example</h1>
+      <button onClick={handleClick}>Click me</button>
+      <div>{text}</div>
+    </div>
+  );
 }
