af5934445076bee80d2f6a61b9ef6e43f9b86bb7
Koji Uegaki <kjugkjk@gmail.com>
2023-05-16 16:17:19 +0900

    route を修正

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -2,11 +2,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
+  const key = process.env.API_KEY;
 
   return NextResponse.json({
     text: `This is a response text from server. You sent ${searchParams.get(
       "input"
     )} `,
-    key: process.env.API_KEY,
+    key,
   });
 }
